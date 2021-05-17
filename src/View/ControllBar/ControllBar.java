78e173726715f43ff8d0484a9edfb1cc6a4f09e4
roy nahmuka <roy.nahmuka@gmail.com>
2021-05-17 15:10:20 +0300

    Without divide the main fxml

--- src/View/ControllBar/ControllBar.java
+++ src/View/ControllBar/ControllBar.java
@@ -1,55 +1,55 @@
-package View.ControllBar;
-
-import Algorithms.TimeSeries;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class ControllBar extends AnchorPane{
-
-    @FXML private ListView features;
-    @FXML private TextField speed;
-
-    public ControllBar() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ControllBar.fxml"));
-        fxmlLoader.setController(this);
-        fxmlLoader.setRoot(this);
-
-        try {
-            fxmlLoader.load();
-        } catch (IOException exc) {
-        }
-    }
-
-
-    public void openHandler() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open CSV File");
-        File file = fileChooser.showOpenDialog(null);
-
-        TimeSeries timeSeries = new TimeSeries(file.getPath());
-        for (String str : timeSeries.features) {
-            features.getItems().add(str);
-        }
-    }
-
-    public void moveForwardHandler() {
-//        simulatorSpeed.setValue(10.0);
-        speed.appendText("1.5");
-    }
-
-    public void moveBackwardHandler() {
-//        simulatorSpeed.setValue(0.1);
-        speed.appendText("0.5");
-    }
-}
+//package View.ControllBar;
+//
+//import Algorithms.TimeSeries;
+//import javafx.fxml.FXML;
+//import javafx.fxml.FXMLLoader;
+//import javafx.fxml.Initializable;
+//import javafx.scene.control.ListView;
+//import javafx.scene.control.TextField;
+//import javafx.scene.layout.AnchorPane;
+//import javafx.stage.FileChooser;
+//import javafx.stage.Stage;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.net.URL;
+//import java.util.ResourceBundle;
+//
+//public class ControllBar extends AnchorPane{
+//
+//    @FXML private ListView features;
+//    @FXML private TextField speed;
+//
+//    public ControllBar() throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ControllBar.fxml"));
+//        fxmlLoader.setController(this);
+//        fxmlLoader.setRoot(this);
+//
+//        try {
+//            fxmlLoader.load();
+//        } catch (IOException exc) {
+//        }
+//    }
+//
+//
+//    public void openHandler() {
+//        FileChooser fileChooser = new FileChooser();
+//        fileChooser.setTitle("Open CSV File");
+//        File file = fileChooser.showOpenDialog(null);
+//
+//        TimeSeries timeSeries = new TimeSeries(file.getPath());
+//        for (String str : timeSeries.features) {
+//            features.getItems().add(str);
+//        }
+//    }
+//
+//    public void moveForwardHandler() {
+////        simulatorSpeed.setValue(10.0);
+//        speed.appendText("1.5");
+//    }
+//
+//    public void moveBackwardHandler() {
+////        simulatorSpeed.setValue(0.1);
+//        speed.appendText("0.5");
+//    }
+//}
--- src/View/MainController.java
+++ src/View/MainController.java
@@ -7,7 +7,9 @@ import javafx.beans.property.*;
 
 import javafx.fxml.FXML;
 import java.io.File;
+import java.io.IOException;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -21,5 +23,27 @@ public class MainController {
 
 
     private DoubleProperty simulatorSpeed;
+    @FXML private ListView features;
+    @FXML private TextField speed;
 
+    public void openHandler() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open CSV File");
+        File file = fileChooser.showOpenDialog(null);
+
+        TimeSeries timeSeries = new TimeSeries(file.getPath());
+        for (String str : timeSeries.features) {
+            features.getItems().add(str);
+        }
+    }
+
+    public void moveForwardHandler() {
+//        simulatorSpeed.setValue(10.0);
+        speed.appendText("1.5");
+    }
+
+    public void moveBackwardHandler() {
+//        simulatorSpeed.setValue(0.1);
+        speed.appendText("0.5");
+    }
 }
